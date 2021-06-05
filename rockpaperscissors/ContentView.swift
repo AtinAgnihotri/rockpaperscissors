71bfb6f47c8f7622faa7ed0d20c968973f4da166
Atin Agnihotri <atinagnihotri@gmail.com>
2021-06-05 21:54:44 +0530

    Add Scorekeeping

--- rockpaperscissors/ContentView.swift
+++ rockpaperscissors/ContentView.swift
@@ -8,9 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shouldWin = false
+    private var rolls = ["Rock", "Paper", "Scissors"]
+    @State private var moves = 1
+    @State private var currentRoll = 0
+    @State private var score = 0
+    @State private var userAns = ""
+    @State private var showScore = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("Your score is \(score)")
+                .padding()
+                .border(Color.black, width: 1)
+            Spacer()
+            Text(shouldWin ? "Win against a \(rolls[currentRoll])" : "Lose against a \(rolls[currentRoll])")
+                .padding()
+            
+            Spacer()
+            VStack {
+                ForEach(0..<rolls.count) { count in
+                    Button (rolls[count]){
+                        nextMove(userAns: rolls[count])
+                    }
+                }
+                    .frame(minWidth: 20, idealWidth: 20, maxWidth: .infinity, minHeight: 20, idealHeight: 20, maxHeight: .infinity, alignment: .center)
+                    .border(Color.black, width: 1)
+            }
+        }.alert(isPresented: $showScore) {
+            Alert(title: Text("Your Score"), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) )
+        }
+    }
+    
+    func nextMove(userAns: String) {
+        scoreKeeping(userAns: userAns)
+        if moves < 10 {
+            moves += 1
+            shouldWin = Bool.random()
+            currentRoll = Int.random(in: 0...2)
+        } else {
+            showScore = true
+        }
+    }
+    
+    func rollPromptResolver(roll: String) -> String{
+        switch roll {
+            case "Rock":
+                return shouldWin ? "Paper" : "Scissors"
+            case "Paper":
+                return shouldWin ? "Scissors" : "Rock"
+            case "Scissors":
+                return shouldWin ? "Rock" : "Paper"
+            default:
+                return "-1"
+        }
+    }
+    
+    func scoreKeeping(userAns: String) {
+        let correctAns = rollPromptResolver(roll: rolls[currentRoll])
+        if userAns == correctAns {
+            score += 1
+        }
     }
 }
 
