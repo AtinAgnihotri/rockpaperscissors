57d8867f2cb1615bdc7d7262ad4831e3c1224814
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-05 18:23:21 +0530

    Initial Commit

---
+++ rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by Atin Agnihotri on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
